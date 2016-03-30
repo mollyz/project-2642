6f10b3f78b137f3a96ff2d1c3bc1e3f637856047
Mikael Juntti <juntti.mikael@gmail.com>
2016-03-30 20:50:28 +0200

    login controller
    builds a querystring and redirects to spotify authorization screen
    (login prompt) which the returns a “code” string which we use to
    retrieve the access-token

---
+++ app/js/loginCtrl.js
@@ -0,0 +1,20 @@
+playlistApp.controller('LoginCtrl', function ($scope,Playlist) {
+
+  // TODO in Lab 5: you will need to implement a method that searchers for dishes
+  // including the case while the search is still running.
+  $scope.login = function (){
+  	var client_id = "7c6e405a8e084d6488c5bb0e892c52ba";
+  	var scope = 'user-read-private user-read-email playlist-read-private';
+    //var redirect_uri = 'http%3A%2F%2Fwww.aftonbladet.se';
+  	var redirect_uri = 'http%3A%2F%2Flocalhost%2Fproject%2Fapp%2Findex.html%23%2Fsearch';
+
+  	var querystring = "client_id="+client_id+"&response_type=code&redirect_uri="+redirect_uri+"&scope="+scope;
+  	console.log("QUERY: " +querystring);
+    window.location.href='https://accounts.spotify.com/authorize?' + querystring;
+
+    //var foo = Playlist.getQueryString('token');
+  	//res.redirect('https://accounts.spotify.com/authorize?' + querystring);
+
+  }
+
+});
