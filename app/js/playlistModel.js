408594b43d98d6634d72ae172ae762c5e2c47d73
Mikael Juntti <juntti.mikael@gmail.com>
2016-04-04 22:14:39 +0200

    more methods
    methods to get songs within a playlist and user info added. also some
    cookie functionality that stores the user_id and access_token

--- app/js/playlistModel.js
+++ app/js/playlistModel.js
@@ -1,34 +1,55 @@
-playlistApp.factory('Playlist',function ($cookieStore,$resource) {
-  
+playlistApp.factory('Playlist',function ($cookieStore,$resource,$http) {
+
   var accessToken = "";
+  var userName = "";
   //var dishes = [];
 
-  	
-  //this.PlaylistSearch = $resource('https://api.spotify.com/v1/users/ledzappa/playlists');
+  this.getUserPlaylists = function(accessToken) {
+    $.ajax({
+      url: 'https://api.spotify.com/v1/users/ledzappa/playlists?limit=50',
+      headers: {
+       'Authorization': 'Bearer ' + accessToken
 
-	this.getUserPlaylists = function(accessToken) {
-        $.ajax({
-            url: 'https://api.spotify.com/v1/users/ledzappa/playlists',
-            headers: {
-               'Authorization': 'Bearer ' + accessToken
+     },
+     dataType: 'json',
+     success: function(result){
+      $("#results").html("");
 
-            },
-            dataType: 'json',
-            success: function(result){
-            $("#results").html("");
+      var result = result.items;
+      for (key in result){
+       var playlist = result[key];
+           /* for (key in playlist.images){
+              var image = playlist.images[key]
+              console.log(image.url);
+            }*/
+            $("#results").append("<div id='playlist-div' style='background-image: url("+playlist.images[0].url+");'><a href='#/playlist/"+playlist.id+"/"+playlist.owner.id+"'><span id='playlist-div-span'>"+playlist.name+"</span></a></div>");
+          }
 
-      		var result = result.items;
-      		for (key in result){
-		    	var playlist = result[key];
-		    	$("#results").append("<div id='playlist-div'><div id='playlist-div-1'></div><div id='playlist-div-2'></div><div id='playlist-div-3'></div><div id='playlist-div-4'></div><p>Name:"+playlist.name+"</p><p>ID: "+playlist.id+"</p></div>");
-		      	}
+          return result.items;
+        }
+      });
+  }
 
-      		return result.items;
+  /*this.getPlaylistTracks = function(playlistid) {
+    $.ajax({
+      url: 'https://api.spotify.com/v1/users/ledzappa/playlists/'+playlistid+'/tracks',
+      headers: {
+       'Authorization': 'Bearer ' + this.getAccessToken()
+
+     },
+     dataType: 'json',
+     success: function(result){
+
+      var result = result.items;
+      var name = result["1"].track.name;
+      console.log(name);
+      return name;
     }
-        });
-    }
-
-	this.getUserData = function(accessToken) {
+    
+  });
+  }
+  */
+	/*this.getUserData = function(accessToken) {
         $.ajax({
             url: 'https://api.spotify.com/v1/me',
             headers: {
@@ -39,36 +60,78 @@ playlistApp.factory('Playlist',function ($cookieStore,$resource) {
       console.log("Playlists:"+ result);
     }
         });
+      }*/
+
+      this.getUserData = function(){
+        $http({
+          method: 'GET',
+          url: 'https://api.spotify.com/v1/me',
+          headers: {'Authorization': 'Bearer ' + this.getAccessToken()}
+        }).then(function successCallback(response) {
+// this callback will be called asynchronously
+// when the response is available
+var data = response.data;
+var name = data.display_name;
+userName = name;
+console.log(data.display_name);
+$cookieStore.put("userid", data.id);
+return name;
+}, function errorCallback(response) {
+// called asynchronously if an error occurs
+// or server returns response with an error status.
+});
+      }
+
+   this.getPlaylistTracks = function(playlistid){
+      return $http({
+      method: 'GET',
+      url: 'https://api.spotify.com/v1/users/ledzappa/playlists/'+playlistid+'/tracks',
+      headers: {'Authorization': 'Bearer ' + this.getAccessToken()}
+    }).then(function(response) {
+      // this callback will be called asynchronously
+      // when the response is available
+      var data = response.data.items;
+      console.log("PlaylistTRACKresponse: "+data);
+      return data;
+      }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      });
     }
 
-  this.getQueryString = function(name, url) {
-  	console.log("halleluja");
-    if (!url) url = window.location.href;
+
+    this.getQueryString = function(name, url) {
+     console.log("halleluja");
+     if (!url) url = window.location.href;
     //url = url.toLowerCase(); // This is just to avoid case sensitiveness  
     name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
+    results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 
-	}
+  }
 
-	this.setAccessToken = function(token){
-		accessToken = token;
-		$cookieStore.put("accesstoken", token);
-		console.log("accesstoken set");
-	};
+  this.setAccessToken = function(token){
+    accessToken = token;
+    $cookieStore.put("accesstoken", token);
+    console.log("accesstoken set");
+  };
 
-	this.getAccessToken = function(){
-		if($cookieStore.get("accesstoken")){
-      	return $cookieStore.get("accesstoken");
-    }else{
-      	return accessToken;
-    }
-	}
+  this.getAccessToken = function(){
+    if($cookieStore.get("accesstoken")){
+     return $cookieStore.get("accesstoken");
+   }else{
+     return accessToken;
+   }
+ }
 
-  
+ this.getUserName = function(){
+  return $cookieStore.get("userid");
+}
+
+
 
 
   // TODO in Lab 5: Add your model code from previous labs
