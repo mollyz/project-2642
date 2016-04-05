217c38d777d818c605801b846ebfbd18f148f0b7
Mikael Juntti <juntti.mikael@gmail.com>
2016-04-06 01:04:32 +0200

    php to return playlist-ids
    returns playlist-id’s from the database with a specific genre. should
    be appended with functionality to return id’s according to specific
    moods as well.

---
+++ app/getplaylistfromgenre.php
@@ -0,0 +1,39 @@
+<?php
+$username = "root";
+$password = "";
+$servername = "localhost";
+$databasename = 'playlist';
+//$info = '000003 03030303 030303030 030303030 030303030';
+//$info = $_POST['Result'];
+//$name = $_POST['Name'];
+//$antalkassa = $_POST['Antalkassa'];
+
+$genre = $_POST['Genre'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $databasename);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+//$sql = 'SELECT mood,genre,keywords WHERE id="$id"';
+$sql = "SELECT id FROM playlist WHERE genre='$genre'";
+$result = $conn->query($sql);
+$array = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    	//header('Content-Type: application/json');
+    	//echo json_encode($row);
+    	$array[] = $row;
+        //echo "id: " . $row["id"]. " - mood: " . $row["mood"]. " " . $row["genre"]. "<br>";
+    }
+     echo json_encode($array);
+
+} else {
+    echo "0 results";
+}
+
+?>
