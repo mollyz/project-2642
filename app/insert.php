a6b2a8d9fa8008a3cab297cb7878dabb49f37a0a
Mikael Juntti <juntti.mikael@gmail.com>
2016-04-18 10:42:15 +0200

    updates

--- app/insert.php
+++ app/insert.php
@@ -1,6 +1,6 @@
 <?php
 $username = "root";
-$password = "root";
+$password = "";
 $servername = "localhost";
 $databasename = 'playlist';
 //$info = '000003 03030303 030303030 030303030 030303030';
@@ -22,8 +22,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO playlist (id, mood, genre, keywords)
-VALUES ('$id', '$mood', '$genre', '$keywords')";
+//$sql = "INSERT INTO playlist (id, mood, genre, keywords)
+//VALUES ('$id', '$mood', '$genre', '$keywords')";
+
+$sql = "INSERT INTO playlist (id, mood, genre, keywords) VALUES('$id', '$mood', '$genre', '$keywords') ON DUPLICATE KEY UPDATE    
+mood='$mood', genre='$genre', keywords='$keywords'";
 
 echo $sql;
 
