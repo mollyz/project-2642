094dad9d692fa87854d51361f454a17193bbe6b7
Mikael Juntti <juntti.mikael@gmail.com>
2016-04-22 23:12:03 +0200

    added sliding animations for the playlists in the search view and some other stuff

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,71 +1,132 @@
-playlistApp.controller('SearchCtrl', function ($scope,Playlist) {
+playlistApp.controller('SearchCtrl', function ($scope,$compile,Playlist) {
 
 
-  $scope.search = function(){
-  	console.log("search");
-  	$scope.playlists = Playlist.getUserPlaylists(Playlist.getAccessToken(),$scope.getAllPlaylistsFromDatabase());
-  	console.log($scope.playlists);
-  }
+  $('.span-moods').on('click', function(){
+    $('.span-moods').removeClass('currentLink');
+    $(this).addClass('currentLink');
+    console.log("addingclass");
+});
 
-  //searches by genre
+  //search function that searches by genre OR mood (query)
+  //gets all the playlists from the backend with the corresponding mood/genre
   $scope.searchGenreMood = function(query){
-    //console.log(query);
+    var userId = Playlist.getUserId();
     $.ajax({
       type: 'POST',
       url: 'getplaylistfromgenre.php',
       dataType: 'json',
-      data: {Query:query},
+      data: {Query:query, UserId:userId},
       success: function(result){
-        //console.log("result");
-        //console.log(result);
         var array = []
+        console.log("RESULT! "+result);
         for(key in result){
           array.push(result[key].id);
         }
-        //console.log(array);
         Playlist.getPlaylist(array,query);
         Playlist.searchPlaylists(query);
-
-
       },
       error: function(){
         $("#results").html("<span class='errormsg'>You don't have any playlists tagged as '"+query+"'. Check out the ones below!</span>");
         Playlist.searchPlaylists(query);
-
-
       }
-
     });
   }
 
-  //gets all of users playlists
-  //gets an array of id's in DB 
-  $scope.getAllPlaylists = function(){
-    //console.log(query);
+
+  //gets all playlists that the user HAS ADDED META to
+  //and put them in an array, then compares the array to
+  //an array containing ALL THE USERS PLAYLISTS in order to
+  //know where to put the "edited" marker. Then we're generating
+  //the output html where we also add ng-directives for slide-animations.
+  $scope.getAllPlaylists = function(userId){
+    console.log("getaUSERID "+userId);
     $.ajax({
       type: 'POST',
       url: 'getplaylist.php',
       dataType: 'json',
+      data: {UserId:userId},
       success: function(result){
-        console.log("result get ALL");
+        console.log("result get ALL from database");
         console.log(result);
         var array = []
         for(key in result){
           array.push(result[key].id);
         }
-      Playlist.getUserPlaylists(Playlist.getAccessToken(),array)
+        Playlist.getUserPlaylists(Playlist.getAccessToken()).then(function(data){
+        console.log(data);
+        $("#results").html("<span class='header-style1'>Your playlists:</span><br />");
+
+        playlists = data.items;
+        Playlist.setPlaylists(playlists);
+        console.log(playlists.id);
+        for (key in playlists){
+        var playlist = playlists[key];
+        var editedhtml="";
+          if(jQuery.inArray(playlist.id, array) !== -1){
+            editedhtml="<div class='div-edited' style='background-color: yellow; margin-top: 162px; margin-left: 83px; position: absolute; width:5px; height:5px; border-width: 1px; border-color: #000; border-radius: 5px;'></div>";
+          }
+            var $el = $("#results").append("<a href='#/playlist/"+playlist.id+"/"+playlist.owner.id+"/"+playlist.name+"'><div class='playlist-div' style='background-image: url("+playlist.images[0].url+");'><div class='box-container' ng-mouseover='slideUp($event)' ng-mouseout='slideDown($event)'></div>"+editedhtml+"<div class='playlist-div-details'>"+playlist.name+" <br/><span='tracks' style='font-size:26px; font-weight:bold'>"+playlist.tracks.total+" Tracks!</span></div></div></a>");
+          }
+          //since we are generating ng-directives dynamically we have to use $compile
+          $compile($el)($scope);
+
+          });
       },
       error: function(){
         console.log("ERROR!");
-
       }
-
     });
   }
 
 
+  //gets the user-specific labels from the backend
+  $scope.getUserLabels = function(userId,labeltype){
+    console.log("getaUSERID "+userId);
+    $.ajax({
+      type: 'POST',
+      url: 'getlabels.php',
+      dataType: 'json',
+      data: {UserId:userId, LabelType: labeltype},
+      success: function(result){
+        if(labeltype=='mood'){
+          $("#search-moods").html('<span class="span-label">Select mood:</span>');
+          for(key in result){
+            var $el = $("#search-moods").append('<span class="span-moods" ng-click="searchGenreMood('+"'"+result[key].mood+"'"+')">'+result[key].mood+'</span>');
+          }
+          $compile($el)($scope);
+        }else if(labeltype=='genre'){
+          $("#search-genres").html('<span class="span-label">Select genre:</span>');
+          for(key in result){
+            var $el = $("#search-genres").append('<span class="span-moods" ng-click="searchGenreMood('+"'"+result[key].genre+"'"+')">'+result[key].genre+'</span>');
+          }
+          $compile($el)($scope);
+        }
+      },
+      error: function(){
+        console.log("ERROR!");
+      }
+    });
+  }
+
+  //some nice animations using jQuery.animate()
+  //triggered when hovering above the playlists in the search view
+  $scope.slideUp = function($event){
+    console.log("slide");
+    var a = jQuery($event.target)
+    console.log(a.parent());
+    //a.parent().find('.playlist-div-details').css('margin-top', '0px');
+    a.parent().find('.playlist-div-details').animate({'margin-top': '0px'}, 150);
+  }
+    $scope.slideDown = function($event){
+    var a = jQuery($event.target);
+    a.parent().find('.playlist-div-details').animate({'margin-top': '130px'}, 150);
+  }
+
+  //we get the user-details and all the user's playlists everytime we load this view
   Playlist.getUserData();
-  $scope.getAllPlaylists();
+  $scope.getUserLabels(Playlist.getUserId(),"mood");
+  $scope.getUserLabels(Playlist.getUserId(),"genre");
+  $scope.getAllPlaylists(Playlist.getUserId());
 
 
 });
