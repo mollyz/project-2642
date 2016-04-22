4daa1ad70a1c8fc2ddefebba376eaceedf822156
Mikael Juntti <juntti.mikael@gmail.com>
2016-04-22 23:13:12 +0200

    added some tooltip functionality for the header

--- app/js/headerCtrl.js
+++ app/js/headerCtrl.js
@@ -1,6 +1,9 @@
 playlistApp.controller('HeaderCtrl', function ($scope,Playlist) {
   console.log("headerCTRL");
   //$scope.username = Playlist.getUserName();
+$(document).ready(function(){
+    $("a.tooltipLink").tooltip(); 
+});
 
   $scope.getUserinfo = function(){
 		//console.log("getting user details..");
@@ -10,6 +13,13 @@ playlistApp.controller('HeaderCtrl', function ($scope,Playlist) {
 			});
 	}
 
+/*
+	$('.glyphicon-question-sign').qtip({ // Grab some elements to apply the tooltip to
+    content: {
+        text: 'My common piece of text here'
+    }
+	});
+*/
   $scope.getUserinfo();
 
 });
