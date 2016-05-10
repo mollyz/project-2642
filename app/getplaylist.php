99eeaacac872618e133f4ded1362158472531d01
Mikael Juntti <juntti.mikael@gmail.com>
2016-05-10 18:54:31 +0200

    Architecture fixes
    Http instead of ajax

--- app/getplaylist.php
+++ app/getplaylist.php
@@ -1,14 +1,15 @@
 <?php
 include 'db-login.php';
-//$info = '000003 03030303 030303030 030303030 030303030';
-//$info = $_POST['Result'];
-//$name = $_POST['Name'];
-//$antalkassa = $_POST['Antalkassa'];
 
-$id = $_POST['Id'];
-$userid = $_POST['UserId'];
+//$id = $_POST['Id'];
+//$userid = $_POST['UserId'];
 //$userid = "ledzappa";
 //echo "SELECT id FROM pl_".$userid."_playlists";
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+$id = $request->Id;
+$userid = $request->UserId;
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $databasename);
--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,4 +1,4 @@
-playlistApp.controller('SearchCtrl', function ($scope,$compile,Playlist) {
+playlistApp.controller('SearchCtrl', function ($scope,$compile,Playlist,$http) {
 
 
   $('.span-moods').on('click', function(){
@@ -10,53 +10,55 @@ playlistApp.controller('SearchCtrl', function ($scope,$compile,Playlist) {
   //search function that searches by genre OR mood (query)
   //gets all the playlists from the backend with the corresponding mood/genre
   $scope.searchGenreMood = function(query){
+    console.log("SearchGenreMood");
     var userId = Playlist.getUserId();
-    $.ajax({
-      type: 'POST',
+    $http({
+      method: 'POST',
       url: 'getplaylistfromgenre.php',
-      dataType: 'json',
-      data: {Query:query, UserId:userId},
-      success: function(result){
-        var array = []
-        console.log("RESULT! "+result);
-        for(key in result){
-          array.push(result[key].id);
-        }
-        Playlist.getPlaylist(array,query);
-        Playlist.searchPlaylists(query);
-      },
-      error: function(){
+      data: {Query:query, UserId:userId}
+    }).then(function SuccessCallback(response){
+      var result = response.data;
+      var array = [];
+      console.log("RESULT! "+result);
+      for(key in result){
+        array.push(result[key].id);
+      }
+      Playlist.getPlaylist(array,query);
+      Playlist.searchPlaylists(query);
+      if(result == 'zeroResults'){
         $("#results").html("<span class='errormsg'>You don't have any playlists tagged as '"+query+"'. Check out the ones below!</span>");
         Playlist.searchPlaylists(query);
       }
+    }, function errorCallback(response){
+      console.log("An error occured");
     });
   }
 
+  //SEARCHES FOR USERS PLAYLIST WITH A KEYWORD AS PARAMETER
   $scope.searchKeywords = function(query){
       var userId = Playlist.getUserId();
-
       console.log(query);
-      $.ajax({
-      type: 'POST',
-      url: 'getplaylistfromkeywords.php',
-      dataType: 'json',
-      data: {Keyword:query, UserId:userId},
-      success: function(result){
-                var array = []
+      $http({
+        method: 'POST',
+        url: 'getplaylistfromkeywords.php',
+        data: {Keyword:query, UserId:userId}
+      }).then(function SuccessCallback(response){
+        var result = response.data;
+        var array = [];
         console.log("RESULT! "+result);
         for(key in result){
           array.push(result[key].id);
         }
         Playlist.getPlaylist(array,query);
         Playlist.searchPlaylists(query);
-      },
-      error: function(){
-        $("#results").html("<span class='errormsg'>You don't have any playlists with the keyword '"+query+"'. Check out the ones below!</span>");
-        Playlist.searchPlaylists(query);
-      }
-    });
-  }
-
+        if(result == 'zeroResults'){
+          $("#results").html("<span class='errormsg'>You don't have any playlists with the keyword '"+query+"'. Check out the ones below!</span>");
+          Playlist.searchPlaylists(query);
+        }
+      }, function errorCallback(response){
+        console.log("An error occurred");
+      })
+    }
 
   //gets all playlists that the user HAS ADDED META to
   //and put them in an array, then compares the array to
@@ -65,42 +67,38 @@ playlistApp.controller('SearchCtrl', function ($scope,$compile,Playlist) {
   //the output html where we also add ng-directives for slide-animations.
   $scope.getAllPlaylists = function(userId){
     console.log("getaUSERID "+userId);
-    $.ajax({
-      type: 'POST',
+    $http({
+      method: 'POST',
       url: 'getplaylist.php',
-      dataType: 'json',
-      data: {UserId:userId},
-      success: function(result){
-        console.log("result get ALL from database");
-        console.log(result);
-        var array = []
-        for(key in result){
-          array.push(result[key].id);
-        }
-        Playlist.getUserPlaylists(Playlist.getAccessToken()).then(function(data){
-        console.log(data);
-        $("#results").html("<span class='header-style1'>Your playlists:</span><br />");
-
-        playlists = data.items;
-        Playlist.setPlaylists(playlists);
-        console.log(playlists.id);
-        for (key in playlists){
-        var playlist = playlists[key];
-        var editedhtml="";
-          if(jQuery.inArray(playlist.id, array) !== -1){
-            editedhtml="<div class='div-edited' style='background-color: yellow; margin-top: 162px; margin-left: 83px; position: absolute; width:5px; height:5px; border-width: 1px; border-color: #000; border-radius: 5px;'></div>";
-          }
-            var $el = $("#results").append("<a href='#/playlist/"+playlist.id+"/"+playlist.owner.id+"/"+playlist.name+"'><div class='playlist-div' style='background-image: url("+playlist.images[0].url+");'><div class='box-container' ng-mouseover='slideUp($event)' ng-mouseout='slideDown($event)'></div>"+editedhtml+"<div class='playlist-div-details'>"+playlist.name+" <br/><span='tracks' style='font-size:26px; font-weight:bold'>"+playlist.tracks.total+" Tracks!</span></div></div></a>");
-          }
-          //since we are generating ng-directives dynamically we have to use $compile
-          $compile($el)($scope);
-
-          });
-      },
-      error: function(){
-        console.log("ERROR!");
+      data: {UserId:userId}
+    }).then(function SuccessCallback(response){
+      var result = response.data;
+      console.log("result get ALL from database");
+      console.log(result);
+      var array = [];
+      for(key in result){
+        array.push(result[key].id);
       }
-    });
+      Playlist.getUserPlaylists(Playlist.getAccessToken()).then(function(data){
+      console.log(data);
+      $("#results").html("<span class='header-style1'>Your playlists:</span><br />");
+      playlists = data.items;
+      Playlist.setPlaylists(playlists);
+      console.log(playlists.id);
+      for (key in playlists){
+      var playlist = playlists[key];
+      var editedhtml="";
+        if(jQuery.inArray(playlist.id, array) !== -1){
+          editedhtml="<div class='div-edited' style='background-color: yellow; margin-top: 162px; margin-left: 83px; position: absolute; width:5px; height:5px; border-width: 1px; border-color: #000; border-radius: 5px;'></div>";
+        }
+        var $el = $("#results").append("<a href='#/playlist/"+playlist.id+"/"+playlist.owner.id+"/"+playlist.name+"'><div class='playlist-div' style='background-image: url("+playlist.images[0].url+");'><div class='box-container' ng-mouseover='slideUp($event)' ng-mouseout='slideDown($event)'></div>"+editedhtml+"<div class='playlist-div-details'>"+playlist.name+" <br/><span='tracks' style='font-size:26px; font-weight:bold'>"+playlist.tracks.total+" Tracks!</span></div></div></a>");
+      }
+        //since we are generating ng-directives dynamically we have to use $compile
+      $compile($el)($scope); 
+    }, function errorCallback(response){
+        console.log("An error occurred");
+      })
+    })
   }
 
 
