f57558a60ffe415b750f05c37de830a3d8198e57
ledzappa <juntti.mikael@gmail.com>
2016-05-13 09:10:26 +0200

    Delete db-login.php

--- app/db-login.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Login details for database connection
-$username = "mikaeljuntti_se";
-$password = "GbotwmTJ";
-$servername = "mikaeljuntti.se.mysql";
-$databasename = 'mikaeljuntti_se';
-?>
